293fd8508c6121fdfed9eada24b1b3a1d630e3ba
TaehwanGo <gth1123@naver.com>
2021-05-12 23:49:32 +0900

    level 1-12

---
+++ javascript/Level1/12/main.js
@@ -0,0 +1,18 @@
+const printDiv = document.querySelector('.print');
+
+/**
+ * 수박수
+ * 3이면 수박수, 4면 수박수박, 5면 수박수박수
+ */
+function solution(n) {
+  return '수박'.repeat(n).substr(0, n);
+}
+
+const answer = solution(11);
+
+console.log(answer);
+
+printDiv.innerHTML = answer;
+
+// 'String'.repeat(n) : n만큼 String을 반복
+// 'String'.substr(n, m) : 'String'에서 n번째 부터 m번째 문자까지 return
