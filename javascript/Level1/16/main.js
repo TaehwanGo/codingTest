b6b962bd94fd5ba40d3830ec6b1e4cc1d4679dde
TaehwanGo <gth1123@naver.com>
2021-05-18 00:05:44 +0900

    1-16 fix

--- javascript/Level1/16/main.js
+++ javascript/Level1/16/main.js
@@ -6,14 +6,17 @@ const printDiv = document.querySelector('.print');
  * hello -> hElLo
  */
 function solution(s) {
-  return s.split(' ').map(word =>
-    word
-      .split('')
-      .map((char, index) =>
-        index % 2 ? char.toLowerCase() : char.toUpperCase(),
-      )
-      .join(' '),
-  );
+  return s
+    .split(' ')
+    .map(word =>
+      word
+        .split('')
+        .map((char, index) =>
+          index % 2 === 0 ? char.toLowerCase() : char.toUpperCase(),
+        )
+        .join(''),
+    )
+    .join(' ');
 }
 
 const answer = solution('try hello world');
