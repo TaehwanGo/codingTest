b5a6e063f5ba559d79f86767241e924eaf987ca4
TaehwanGo <gth1123@naver.com>
2021-05-17 23:59:09 +0900

    1-16

---
+++ javascript/Level1/16/main.js
@@ -0,0 +1,25 @@
+const printDiv = document.querySelector('.print');
+
+/**
+ * 이상한 문자 만들기
+ * 단어의 짝수번째 문자는 대문자로, 홀수번째 문자는 소문자로 바꿉니다.
+ * hello -> hElLo
+ */
+function solution(s) {
+  return s.split(' ').map(word =>
+    word
+      .split('')
+      .map((char, index) =>
+        index % 2 ? char.toLowerCase() : char.toUpperCase(),
+      )
+      .join(' '),
+  );
+}
+
+const answer = solution('try hello world');
+
+console.log(answer);
+
+printDiv.innerHTML = answer;
+
+//
