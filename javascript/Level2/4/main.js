247ed9239f8e2fa8b54d7b67ed13be400ca072b0
TaehwanGo <gth1123@naver.com>
2021-10-01 23:59:31 +0900

    2-4 문제

---
+++ javascript/Level2/4/main.js
@@ -0,0 +1,17 @@
+const printDiv = document.querySelector('.print');
+
+/**
+ * 다음 큰 숫자
+ * n보다 크면서 이진수로 변환했을 때 n의 이진수와 1의 개수가 같은 숫자를 찾습니다.
+ * 78(1001110)의 다음 숫자는 83(101011)입니다.
+ */
+
+function solution(s) {}
+
+const answer = solution();
+
+console.log(answer);
+
+printDiv.innerHTML = answer;
+
+//
